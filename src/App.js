cd45eada63b421d3b7df344c8b6b3e98c3cbd264
teefeid <taef_415@hotmail.com>
2021-07-06 00:39:01 +0300

    HW

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import  './App.css'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement, contactAdded } from './action';
+import { contactAdded ,contactDeleted, contactEdit } from './action';
 import { useState } from 'react'
-
-function App() {
-  const counter = useSelector(state => state.counter)
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function App() {
   const contactlist = useSelector(state => state.contactlist)
   const [phone, setPhone] = useState({})
+  const [selected, setSelected] = useState(-1)
   const dispatch = useDispatch();
   const handelChange = (event) => {
     const att = event.target.name
@@ -14,46 +15,69 @@ function App() {
     updatedValue[att] = value
     console.log("updatedValue", updatedValue);
     setPhone(updatedValue)
-
   }
-
-
   const handelSubmit = (event) => {
     event.preventDefault()
     dispatch(contactAdded(phone))
   }
+  const handelDelete =(id)=>{
+      dispatch(contactDeleted(id));
+      };
+      const handelEdit = (event) => {
+        event.preventDefault()
+        setSelected(-1)
+        dispatch(contactEdit(selected,phone))
+      }
   return (
-    <div >
-      <h1>counter {counter}</h1>
-
-      <button onClick={() => dispatch(increment(2))}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-
-      <ul>{contactlist.map((item, index) => {
+    <div>
+      <center>
+    <h1>Contact List</h1>
+    <ul>{contactlist.map((item, index) => {
+        if(selected === index){
+          return (
+            <form onSubmit={handelEdit}>
+            <label>
+              Number
+              </label><br></br>
+              <input className ="btn btn-info" type="text" name="number" value={phone.number} onChange={handelChange} />
+            <br />
+            <label>
+              Name
+              <input  className ="btn btn-info" type="text" name="name" value={phone.name} onChange={handelChange} />
+            </label>
+            <div>
+            <br />
+              <input className ="btn btn-success" type="submit" value="Sumbit" />
+            </div>
+          </form>
+          )
+        }
+        else{
         return (
           <div key={index}>
-            <li>name {item.name} number {item.number}</li>
-
+            <li className="item">Name {item.name} Number {item.number}</li>
+            <button className ="btn btn-danger" onClick={() => dispatch(contactDeleted(index))}>Delete</button>
+            <button className ="btn btn-warning" onClick={() => {setSelected(index); setPhone(item)}}>Edit</button>
           </div>
-        )
+        )}
       })}</ul>
-
-      <form onSubmit={handelSubmit}>
+     <form onSubmit={handelSubmit}>
         <label>
-          <input type="text" name="number" onChange={handelChange} />
-                number</label>
+          Number
+          </label><br />
+          <input className ="btn btn-info" type="text" name="number" onChange={handelChange} /><br />
         <br />
         <label>
-          <input type="text" name="name" onChange={handelChange} />
-                name</label>
-        <br />
+          Name
+          </label><br />
+          <input  className ="btn btn-info"  type="text" name="name" onChange={handelChange} /> < br/>
         <div>
-          <input type="submit" value="Add to the list" />
+        <br />
+          <input  className ="btn btn-primary" type="submit" value="Add to the list" />
         </div>
       </form>
-
-    </div>
+      </center>
+      </div>
   );
 }
 
-export default App;
