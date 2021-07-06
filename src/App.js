4839839000c806d5bb7c947ccba0b563073baa02
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-07-06 20:45:28 +0300

    add: program files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement, contactAdded } from './action';
+import { increment, decrement, contactAdded , Delete, contactEdited} from './action';
 import { useState } from 'react'
 
 function App() {
   const counter = useSelector(state => state.counter)
   const contactlist = useSelector(state => state.contactlist)
   const [phone, setPhone] = useState({})
+  const [selected, setSelected] = useState(-1)
+  const [editItem, setEditItem] = useState({});
   const dispatch = useDispatch();
+  
   const handelChange = (event) => {
     const att = event.target.name
     const value = event.target.value
@@ -16,6 +19,14 @@ function App() {
     setPhone(updatedValue)
 
   }
+ 
+  const handelEdit = (event) => {
+    event.preventDefault();
+    dispatch(contactEdited(phone,selected));
+    setSelected(-1)
+    setPhone({});
+    
+  }
 
 
   const handelSubmit = (event) => {
@@ -23,7 +34,10 @@ function App() {
     dispatch(contactAdded(phone))
   }
   return (
-    <div >
+    <div style={{margin: "80px auto",
+      width: "50%",
+      border: "1px solid black",
+      padding: "10px"}}>
       <h1>counter {counter}</h1>
 
       <button onClick={() => dispatch(increment(2))}>+</button>
@@ -32,7 +46,24 @@ function App() {
       <ul>{contactlist.map((item, index) => {
         return (
           <div key={index}>
-            <li>name {item.name} number {item.number}</li>
+            <li>name {item.name} number {item.number} 
+             <button onClick={() => dispatch(Delete(index))}>Delete</button>
+             <button onClick={() => { setSelected(index); setPhone(item) }}>Edit</button>
+            {selected === index ?
+              <form onSubmit={handelEdit}>
+                <p>
+                  number:
+                  <input type="text" name="number" value={phone.number} onChange={handelChange} />
+                </p>
+                <p>
+                  name :
+                  <input type="text" name="name" value={phone.name} onChange={handelChange}  />
+                </p>
+                <div>
+                  <input type="submit" value="update" />
+                </div>
+              </form> : null}
+             </li>
 
           </div>
         )
