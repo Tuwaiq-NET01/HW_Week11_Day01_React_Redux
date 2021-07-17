f5423e98d2d67b7d7f2f5dd507ed80d16abc8042
Faisal Abdulaziz <faisal.alsagri.1@gmail.com>
2021-07-17 15:47:09 +0300

    lol

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement, contactAdded } from './action';
-import { useState } from 'react'
+import { increment, decrement, contactAdded, contactDeleted, allowEdit, contactUpdated } from './action';
+import { useState, useEffect } from 'react'
 
 function App() {
   const counter = useSelector(state => state.counter)
@@ -12,16 +12,31 @@ function App() {
     const value = event.target.value
     const updatedValue = { ...phone }
     updatedValue[att] = value
+    updatedValue["isEdited"] = false;
     console.log("updatedValue", updatedValue);
     setPhone(updatedValue)
-
   }
 
+
+  const handelEdit = (event, index) => {
+    event.preventDefault()
+    const object = {
+      number: event.target[0].value,
+      name: event.target[1].value,
+      isEdited: false
+    }
+    dispatch(contactUpdated(index, object))
+  }
+
+  const handleDelete = (index) => {
+    dispatch(contactDeleted(index))
+  }
 
   const handelSubmit = (event) => {
     event.preventDefault()
     dispatch(contactAdded(phone))
   }
+
   return (
     <div >
       <h1>counter {counter}</h1>
@@ -32,8 +47,27 @@ function App() {
       <ul>{contactlist.map((item, index) => {
         return (
           <div key={index}>
-            <li>name {item.name} number {item.number}</li>
+            <li>
+              {
+                item.isEdited ? <form onSubmit={(e) => handelEdit(e, index)}>
+                <label>
+                  <input type="text" name="number" placeholder={item.number} />
+                  number</label>
+                <br />
+                <label>
+                  <input type="text" name="name" placeholder={item.name} />
+                  name</label>
+                <br />
+                <input type="submit" value="Edit" />
+              </form>:
+              <>
+                name {item.name} number {item.number}
+                <button onClick={() => dispatch(allowEdit(index)) }>Edit</button>
+              </>
+              }
 
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
           </div>
         )
       })}</ul>
@@ -41,11 +75,11 @@ function App() {
       <form onSubmit={handelSubmit}>
         <label>
           <input type="text" name="number" onChange={handelChange} />
-                number</label>
+          number</label>
         <br />
         <label>
           <input type="text" name="name" onChange={handelChange} />
-                name</label>
+          name</label>
         <br />
         <div>
           <input type="submit" value="Add to the list" />
--- src/reducers/contactlist.js
+++ src/reducers/contactlist.js
@@ -5,9 +5,15 @@ export default function contactlist(state = [], action) {
                 ...state, {
                     number: action.payload.number,
                     name: action.payload.name,
-
+                    isEdited: action.payload.isEdited
                 }
             ]
+        case "contactDeleted":
+            return state.filter( (ele, index) => index !== action.payload )
+        case "allowEdit":         
+            state[action.payload].isEdited = true; return [...state]
+        case "contactUpdated":
+            state[action.payload.index] = action.payload.body; return [...state]
         default:
             return state
     }
